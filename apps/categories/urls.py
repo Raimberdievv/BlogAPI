61f7eb4b2f9e61117c9c1c5729baa86fa4435533
Raimberdievv <raimberdievnurtilek@gmail.com>
2022-06-20 21:31:37 +0600

    fix bugs

--- apps/categories/urls.py
+++ apps/categories/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path
+from django.urls import path,include
+from django.contrib import admin
 from apps.categories.views import CategoryAPIView,CategoryCreateAPIView,CategoryDeleteAPIView,CategoryUpdateAPIView
 
 urlpatterns = [
     #category api
-    path('categories',CategoryAPIView.as_view(),name = "category_api"),
-    path('category/create',CategoryCreateAPIView.as_view(),name = "category_create_api"),
-    path('category/update/<int:pk>',CategoryUpdateAPIView.as_view(),name = "category_api_update"),
-    path('category/delete/<int:pk>',CategoryDeleteAPIView.as_view(),name = "category_api_delete"),
+    path('admin/',admin.site.urls),
+    path('api-auth/',include('rest_framework.urls')),
+    path('api/categories',CategoryAPIView.as_view(),name = "category_api"),
+    path('api/category/create',CategoryCreateAPIView.as_view(),name = "category_create_api"),
+    path('api/category/update/<int:pk>',CategoryUpdateAPIView.as_view(),name = "category_api_update"),
+    path('api/category/delete/<int:pk>',CategoryDeleteAPIView.as_view(),name = "category_api_delete"),
 ]   
