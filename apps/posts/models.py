a378d8124633712bf92b4737a5660524560863aa
Raimberdievv <raimberdievnurtilek@gmail.com>
2022-06-22 21:09:53 +0600

    add model PostComment

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,4 +1,7 @@
+from tabnanny import verbose
+from venv import create
 from django.db import models
+from apps.users.models import User
 
 # Create your models here.
 
@@ -8,3 +11,16 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title 
+
+class PostComment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)     
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = "Комментарий"    
+        verbose_name_plural = "Комментарии"    
